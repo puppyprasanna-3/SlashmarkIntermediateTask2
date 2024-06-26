2821c655c96d27b3fadb39dc30a59f45bbbdf9fb
puppyprasanna-3 <151060757+puppyprasanna-3@users.noreply.github.com>
2024-06-26 16:35:49 +0530

    Create HttpServletResponse.java

---
+++ Servlet/HttpServletResponse.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import java.io.PrintWriter;
+
+public class HttpServletResponse {
+
+    public void setContentType(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setContentType'");
+    }
+
+    public PrintWriter getWriter() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWriter'");
+    }
+
+    public void setHeader(String string, String string2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setHeader'");
+    }
+}
